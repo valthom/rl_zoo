b27794ad7c35cb9bc1436ffd939e755ebdcd4005
valthom <vathomas@protonmail.ch>
2017-01-29 20:45:22 +0100

    Added reinitialization between each round

--- bandits.py
+++ bandits.py
@@ -43,36 +43,41 @@ class Bandit:
         #self.update_arms()
         return r
 
-    def update_arms(self):
-        """
-        Moves the means of the arms according to a brownian motion
-        """
-        self.means += np.random.standard_normal(self.means.shape)*.0
+    def reinitialize(self):
+        self.means = np.random.randn(K,)*5
+        self.var = np.ones(K,)*3
+
+
+    # def update_arms(self):
+    #     """
+    #     Moves the means of the arms according to a brownian motion
+    #     """
+    #     self.means += np.random.standard_normal(self.means.shape)*.0
 
 
 class Strategy(object):
 
     """Virtual class for all strategies (ubc, eps-greedy, policy gradient..)"""
 
-    def __init__(self, bandit):
-        """TODO: to be defined1. """
-        self.bandit = bandit
-
     def run_round(self, n_round=5, episodes=1000, plot=True):
+        # we compute mean and variance recursively
         M_reward = np.zeros(episodes,) #n x mean
         S_reward = np.zeros(episodes,) #n x var
         for i_round in tqdm(range(1, n_round+1)):
             self.reinitialize()
+            #self.bandit.reinitialize()
             rewards = np.zeros(episodes,)
             for i in range(episodes):
                 # Select action
                 a = self.action()
-
+                # Get reward for that action
                 r = self.bandit.pull(a)
-
+                #r = 100*r/self.bandit.means.max()
+                # Update strategy with given action/reward pair
                 self.observe(a, r)
+
                 rewards[i] = r
-            x = np.cumsum(rewards)/np.arange(1, episodes+1)#, rewards
+            x = np.cumsum(rewards)/np.arange(1, episodes+1)# Rt/t
             M_reward += x
             S_reward += ((i_round)*x - M_reward)**2/(i_round*(i_round+1))
         meanR, stdR = M_reward/n_round, np.sqrt(S_reward/n_round)
@@ -82,18 +87,17 @@ class Strategy(object):
 
     def plot(self, meanR, stdR):
         best_reward = bandit.means.max()
-        meanR, stdR = 100*meanR/best_reward, 100*stdR/best_reward
+        vec = np.arange(episodes)
+        meanR, stdR = 100*meanR/best_reward, 50*stdR/best_reward
         # Plot the average curve and fill 1 sigma around
         plt.plot(vec, meanR, label=self.label())
         plt.fill_between(vec, meanR-stdR, meanR+stdR, alpha = 0.2)
-        #axes.set_ylim([-10, 110])
 
 class UCB(Strategy):
 
     """Class for the UCB algorithm"""
 
     def __init__(self, bandit, c=None, alpha=None):
-        """TODO: to be defined1. """
         self.bandit = bandit
         self.Q = np.ones(bandit.K,)*0
         self.N = np.zeros(bandit.K,)
@@ -173,25 +177,24 @@ class Policy_gradient(Strategy):
         self.alpha = alpha
         self.bandit = bandit
         self.H = np.ones(self.bandit.K,)*0
+        self.proba = np.ones_like(self.H,)/self.H.size
         self.r_m = 0
         self.counter = 0
 
     def action(self):
-        proba = softmax(self.H)
-        a = np.argmax(np.random.multinomial(1, proba))
+        self.proba = softmax(self.H)
+        a = np.argmax(np.random.multinomial(1, self.proba))
         return a
 
     def observe(self, a, r):
         self.r_m = self.counter/(1+self.counter)*self.r_m + 1/(1+self.counter)*r
         self.counter += 1
-        proba = softmax(self.H)
-        self.H[a] += self.alpha*(r - self.r_m)*(1-proba[a])
-        other_indices = [True]*self.bandit.K
-        other_indices[a] = False
-        self.H[other_indices] -= self.alpha*(r - self.r_m)*proba[other_indices]
+        self.H -= self.alpha*(r - self.r_m)*self.proba
+        self.H[a] += self.alpha*(r - self.r_m)
 
     def reinitialize(self):
         self.H *= 0
+        self.proba = np.ones_like(self.H,)/self.H.size
         self.counter = 0
         self.r_m = 0
 
@@ -201,7 +204,7 @@ class Policy_gradient(Strategy):
 
 
 K, episodes = 10, 2000
-n_round = 20
+n_round = 50
 bandit = Bandit(K)
 
 
@@ -214,11 +217,15 @@ e_gr = eps_greedy_value_table(bandit, eps)
 e_gr.run_round(n_round, episodes=episodes)
 
 # Strategy 2
+eps = 0.2
+e_gr2 = eps_greedy_value_table(bandit, eps)
+e_gr2.run_round(n_round, episodes=episodes)
+
+# Strategy 3
 pg = Policy_gradient(bandit)
 pg.run_round(n_round, episodes=episodes)
 
-
-# Strategy 3
+# Strategy 4
 ucb = UCB(bandit)
 ucb.run_round(n_round, episodes=episodes)
 
@@ -226,6 +233,7 @@ ucb.run_round(n_round, episodes=episodes)
 # Add title and axis labels to the plot
 plt.legend(loc='best')
 axes=plt.gca()
+axes.set_ylim([-10, 110])
 plt.xlabel('Episodes $(t)$')
 plt.ylabel('$\mathbb{E} [R_t]$ (as % of best expected reward)')
 plt.title('K-armed bandits $(K={})$'.format(K))
